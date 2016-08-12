aa7b9930d91d761e0c0d154a9466902732212ad6
Pascal Raven <pascal@raventec.cc>
2016-08-12 15:43:37 -0700

    Uploading files
    For Ashton to take a look and fix error!!

--- js/main.js
+++ js/main.js
@@ -11,11 +11,32 @@
 //     });
 // });
 
-function myFunc(name) {
-	alert("hi "+name);
-}
 
 function toggleDetails(div) {
-  document.getElementById(div).style.display = 'block';
-  e.preventDefault();
+	$('#'+div).toggle(500);
 }
+
+function populateOwners(owners){
+	var parent = $('#owner-parent');
+	owners.forEach(function(owner){
+		var details = "";
+		var equipment = "";
+		owner.details.forEach(function(detail){
+			var value = detail.value;
+			if(detail.property === "Email"){
+				value = "<a href='mailto:"+detail.value+"'>"+detail.value+"</a>";
+			}
+			details += "<li><span class='details-bold'>"+detail.property+": </span>"+value;
+			if(detail.property === "Contact" && typeof(detail.cell) !== 'undefined'){
+				details += " <span class='details-bold'>Cell: </span>"+detail.cell;
+			}
+			details += "</li>"
+		});
+		owner.equipment.forEach(function(equipmentItem){
+			equipment += "<li>"+equipmentItem+"</li>";
+		});
+		html = `<div class="row"> <div class="col-sm-12"> <a href="javascript:void(0)" onclick="toggleDetails('${owner.id}');">${owner.name}</a> <div id="${owner.id}" class="owner-details"> Details: <ul> ${details} </ul> Equipment: <ul> ${equipment} </ul> </div> </div> </div>`;
+		parent.append(html);
+	});
+}
+populateOwners(json);
