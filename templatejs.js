7699fc3265b550529449cd321dc71ea2c9671a27
Pascal Raven <pascal@raventec.cc>
2016-08-12 15:39:58 -0700

    Add files via upload

---
+++ templatejs.js
@@ -0,0 +1,37 @@
+{
+  "id": "alltera",
+  "name": "Allterra Construction Ltd.",
+  "details": [
+    {
+      "property": "Contact",
+      "value": "Gary Isacson",
+      "cell": "250-883-5302"
+    },
+    {
+      "property": "Bus",
+      "value": "250-658-3772"
+    },
+    {
+      "property": "Fax",
+      "value": "250-658-4944"
+    },
+    {
+      "property": "Email",
+      "value": "gary@allterraconstruction.ca"
+    },
+    {
+      "property": "Address",
+      "value": "2158 Millstream Road, Victoria, BC V9B 6H4"
+    }
+  ],
+  "equipment": [
+    "Trucks",
+    "Truck with Trailer",
+    "Rubber Tired Backhoes",
+    "Bulldozer Blade",
+    "Compaction Equipment",
+    "Excavators",
+    "Lowbeds",
+    "Notes: Skid Steer, Rollers",
+  ]
+},
