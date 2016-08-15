2192be604190137d2aedcebbd3533540f3091730
AshtonMeuser <ashtonmeuser@gmail.com>
2016-08-14 20:59:26 -0700

    Reformatted dynamic html, style tweaks

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
-// $(function(){
-//     $('[rel="popover"]').popover({
-//         container: 'body',
-//         html: true,
-//         content: function () {
-//             var clone = $($(this).data('popover-content')).clone(true).removeClass('hide');
-//             return clone;
-//         }
-//     }).click(function(e) {
-//         e.preventDefault();
-//     });
-// });
-
+$.getJSON("data/equip-owners.json", function(json) {
+	populateOwners(json);
+});
 
 function toggleDetails(div) {
-	$('#'+div).toggle(500);
+	$('#'+div).slideToggle(200);
 }
 
 function populateOwners(owners){
-	var parent = $('#owner-parent');
+	var container = $('#owner-container');
 	owners.forEach(function(owner){
 		var details = "";
 		var equipment = "";
@@ -25,18 +15,29 @@ function populateOwners(owners){
 			var value = detail.value;
 			if(detail.property === "Email"){
 				value = "<a href='mailto:"+detail.value+"'>"+detail.value+"</a>";
+			}else if(detail.property === "Web"){
+				value = "<a href='http://"+detail.value+"'>"+detail.value+"</a>";
 			}
-			details += "<li><span class='details-bold'>"+detail.property+": </span>"+value;
+			details += "<li><strong>"+detail.property+": </strong>"+value;
 			if(detail.property === "Contact" && typeof(detail.cell) !== 'undefined'){
-				details += " <span class='details-bold'>Cell: </span>"+detail.cell;
+				details += " <strong>Cell: </strong>"+detail.cell;
 			}
 			details += "</li>"
 		});
 		owner.equipment.forEach(function(equipmentItem){
 			equipment += "<li>"+equipmentItem+"</li>";
 		});
-		html = `<div class="row"> <div class="col-sm-12"> <a href="javascript:void(0)" onclick="toggleDetails('${owner.id}');">${owner.name}</a> <div id="${owner.id}" class="owner-details"> Details: <ul> ${details} </ul> Equipment: <ul> ${equipment} </ul> </div> </div> </div>`;
-		parent.append(html);
+		html = "\
+			<div class='row'>\
+				<div class='col-xs-12'>\
+					<a class='owner-title' href='javascript:void(0)' onclick='toggleDetails(\""+owner.id+"\");'>"+owner.name+"</a>\
+					<div id='"+owner.id+"' class='owner-details'>\
+						Details: <ul>"+details+"</ul>\
+						Equipment: <ul>"+equipment+"</ul>\
+					</div>\
+				</div>\
+			</div>\
+		";
+		container.append(html);
 	});
 }
-populateOwners(json);
